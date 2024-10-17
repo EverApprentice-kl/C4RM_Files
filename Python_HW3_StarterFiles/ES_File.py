a2284ceb46b03b575caf3540ed03a074163c2d32
EverApprentice-kl <kl3570@columbia.edu>
2024-10-16 23:15:21 -0400

    Updated ES and VaR

--- Python_HW3_StarterFiles/ES_File.py
+++ Python_HW3_StarterFiles/ES_File.py
@@ -1,5 +1,10 @@
-
+"""
+Date: 10.16.2024
+Author: KL
+Description: Calculate expected shortfall
+"""
 import numpy as np
+
 
 def ES(losses, alpha=None, VaR=None):
     """
@@ -10,6 +15,20 @@ def ES(losses, alpha=None, VaR=None):
     :param VaR: dollar value or percentage specifying the VaR threshold
     :return: Expected Shortfall as the average of losses exceeding VaR
     """
+    # If neither alpha nor VaR is provided
+    if alpha and VaR is None:
+        raise ValueError("Either VaR or alpha must be provided.")
+    elif VaR is None:
+        # Calculate VaR if not provided
+        VaR = np.percentile(losses, 100 * (1-alpha))
 
-    es_value = 90
+    # Get losses that exceed the VaR threshold
+    tail_losses = losses[losses > VaR]
+
+    # Calculate expected shortfall
+    if len(tail_losses) == 0:
+        return 0
+    else:
+        es_value = np.mean(tail_losses)
+        
     return es_value
--- Python_HW3_StarterFiles/VaR_File.py
+++ Python_HW3_StarterFiles/VaR_File.py
@@ -1,10 +1,23 @@
+"""
+Date: 10.16.2024
+Author: KL
+Description: VaR function
+"""
 import numpy as np
+import matplotlib.pyplot as plt
+
 
 def VaR(r, alpha):
     # This function returns the left tail value
     # alpha = risk level
     # r = an array of stock returns
     # out = positively stated value of r at the 1-alpha percentile
+    percentile_value = np.percentile(r, 100 * (1 - alpha))
+    return abs(percentile_value)
 
-    out = .01 # Calculate the percentile
-    return abs(out)  # Return the absolute value of the calculated percentile
+
+# Example
+returns = np.random.normal(0, 1, 10000)
+print(np.percentile(returns, 97.72))
+plt.hist(returns, bins=200, alpha=0.75)
+plt.show()
